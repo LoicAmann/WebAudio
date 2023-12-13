c72464b2d07ec6573db8df2d39f3c0fd536f36c0
OnsHamdi99 <77619552+OnsHamdi99@users.noreply.github.com>
2023-12-13 21:59:08 +0100

    rebase

--- js/script.js
+++ js/script.js
@@ -20,3 +20,26 @@ function init() {
         player.connect(eq.inputNode, eq.outputNode);
     });
 }
+
+// Function to handle the song upload
+function uploadSong() {
+  const fileInput = document.getElementById('fileInput');
+  const file = fileInput.files[0];
+
+  if (file) {
+      const fileName = file.name;
+      const filePath = URL.createObjectURL(file);
+
+      // Assuming you have a function to add a new song to the playlist
+      playlist.addNewSong({ name: fileName, file: filePath, image: 'img/default.png' });
+
+      // You may want to trigger a changeSong event with the new song
+      const newIndex = playlist.songs.length - 1; // Index of the newly added song
+      playlist.changeSong(newIndex);
+  } else {
+      alert('Please select a valid MP3 file.');
+  }
+}
+
+window.uploadSong = uploadSong; 
+window.onload = init;
