07440858f3cd03eec57af260182d1cce6624f514
LoicAmann <amann.loic@gmail.com>
2023-12-23 18:15:22 +0100

    Change balance par stéréo + equalizer

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 window.onload = init;
 
-let playerComponent, player, playlist, controllers, visualizer, echo;
+let playerComponent, player, playlist, controllers, visualizer, equalizer;
 
 function init() {
     console.log("page chargée");
     playerComponent = document.querySelector("my-audio-player");
+    console.log("Player Component récupéré");
     console.log(playerComponent);
     playlist = document.querySelector("#playlist");
+    console.log("Playlist récupérée");
+    console.log(playlist);
     controllers = document.querySelector("#controllers");
+    console.log("Controllers récupérés");
+    console.log(controllers);
+    equalizer = controllers.getEqualizerComponent();
+    console.log("Equalizer récupéré");
+    console.log(equalizer);
 
     //Volume
     controllers.addEventListener('adjustVolume', (event) => {
@@ -34,11 +42,47 @@ function init() {
         playerComponent.deactivateEcho();
     });
 
-    //Balance
-    controllers.addEventListener('adjustBalance', (event) => {
-        console.log("adjustBalance event received");
-        console.log("Set balance value : " + event.detail*100);
-        playerComponent.setBalance(parseInt(event.detail*100));
+    //Stereo
+    controllers.addEventListener('adjustStereo', (event) => {
+        console.log("adjustStereo event received");
+        console.log("Set stereo value : " + event.detail);
+        playerComponent.setStereo(parseInt(event.detail));
+    });
+
+    //Equalizer
+    equalizer.addEventListener('clearEqualizer', (event) => {
+        console.log("clearEqualizer event received");
+        playerComponent.clearEqualizer();
+    });
+    equalizer.addEventListener('adjustFiltre1', (event) => {
+        console.log("adjustFiltre1 event received");
+        console.log("Set filtre1 value : " + event.detail);
+        playerComponent.setFiltre1(parseInt(event.detail));
+    });
+    equalizer.addEventListener('adjustFiltre2', (event) => {
+        console.log("adjustFiltre2 event received");
+        console.log("Set filtre2 value : " + event.detail);
+        playerComponent.setFiltre2(parseInt(event.detail));
+    });
+    equalizer.addEventListener('adjustFiltre3', (event) => {
+        console.log("adjustFiltre3 event received");
+        console.log("Set filtre3 value : " + event.detail);
+        playerComponent.setFiltre3(parseInt(event.detail));
+    });
+    equalizer.addEventListener('adjustFiltre4', (event) => {
+        console.log("adjustFiltre4 event received");
+        console.log("Set filtre4 value : " + event.detail);
+        playerComponent.setFiltre4(parseInt(event.detail));
+    });
+    equalizer.addEventListener('adjustFiltre5', (event) => {
+        console.log("adjustFiltre5 event received");
+        console.log("Set filtre5 value : " + event.detail);
+        playerComponent.setFiltre5(parseInt(event.detail));
+    });
+    equalizer.addEventListener('adjustFiltre6', (event) => {
+        console.log("adjustFiltre6 event received");
+        console.log("Set filtre6 value : " + event.detail);
+        playerComponent.setFiltre6(parseInt(event.detail));
     });
 }
 
